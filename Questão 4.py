25a4145cc9ef4fe096dfb743236898a8e55909cd
Juliaffsilveira <juliaffsilveira@gmail.com>
2026-06-09 13:44:28 -0300

    Add files via upload
    Comandos de Entrada e Saída – Conversor de Temperatura 
    Crie um programa que converta temperatura de Celsius para Fahrenheit.
    Utilize a fórmula:
    𝐹 =9𝐶/5+ 32

---
+++ Questão 4.py
@@ -0,0 +1,8 @@
+celsius = float(input("Digite a temperatura em graus Celsius (°C): "))
+
+# Cálculo respeitando a ordem de precedência dos operadores
+fahrenheit = (celsius * 9) / 5 + 32
+
+print("\n----------------------------------------")
+print(f"{celsius}°C equivalem a {fahrenheit}°F")
+print("----------------------------------------")
